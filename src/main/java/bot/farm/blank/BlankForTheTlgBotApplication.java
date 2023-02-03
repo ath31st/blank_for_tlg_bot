49eccb820a5cb040a00ce04e5de7177eb12fd8f7
Vladimir Olennikov <ath31st@yandex.ru>
2023-02-03 16:05:50 +0300

    Create the project

---
+++ src/main/java/bot/farm/blank/BlankForTheTlgBotApplication.java
@@ -0,0 +1,13 @@
+package bot.farm.blank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlankForTheTlgBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlankForTheTlgBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/bot/farm/blank/BlankForTheTlgBotApplicationTests.java
@@ -0,0 +1,13 @@
+package bot.farm.blank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlankForTheTlgBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
