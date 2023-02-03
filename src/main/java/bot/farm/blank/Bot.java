238bfd95acd9fa73f4dc13a90d04e77c42de807f
Vladimir Olennikov <ath31st@yandex.ru>
2023-02-03 16:57:25 +0300

    add methods into bot class

--- src/main/java/bot/farm/blank/Bot.java
+++ src/main/java/bot/farm/blank/Bot.java
@@ -1,16 +1,25 @@
 package bot.farm.blank;
 
+import bot.farm.blank.service.MessageService;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.objects.CallbackQuery;
+import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
+@Slf4j
 @Getter
 @Setter
+@RequiredArgsConstructor
 public class Bot extends TelegramLongPollingBot {
 
     private String botUsername;
     private String botToken;
+    private final MessageService messageService;
 
     @Override
     public String getBotUsername() {
@@ -24,6 +33,46 @@ public class Bot extends TelegramLongPollingBot {
 
     @Override
     public void onUpdateReceived(Update update) {
+        if (update.hasMessage() && update.getMessage().hasText()) {
+            messageProcessing(update.getMessage());
+        }
 
+        if (update.hasCallbackQuery()) {
+            callBackQueryProcessing(update.getCallbackQuery());
+        }
+    }
+
+    private void messageProcessing(Message message) {
+        String chatId = String.valueOf(message.getChatId());
+        String inputText = message.getText();
+
+        sendTextMessage(chatId, inputText); // change here send text!
+    }
+
+    private void callBackQueryProcessing(CallbackQuery callbackQuery) {
+        String chatId = String.valueOf(callbackQuery.getMessage().getChatId());
+
+        switch (callbackQuery.getData()) {
+            case "/INSERT LINK HERE 1" -> {
+                sendTextMessage(chatId,"INSERT TEXT HERE 1");
+            }
+
+            case "/INSERT LINK HERE 2" -> {
+                sendTextMessage(chatId, "INSERT TEXT HERE 2");
+            }
+        }
+    }
+
+
+    public void sendTextMessage(String chatId, String text) {
+        try {
+            execute(messageService.createMessage(chatId, text));
+        } catch (TelegramApiException e) {
+            if (e.getMessage().endsWith("[403] Forbidden: bot was blocked by the user")) {
+                log.info("User with chatId {} has received the \"inactive\" status", chatId);
+            } else {
+                log.error(e.getMessage());
+            }
+        }
     }
 }
--- src/main/java/bot/farm/blank/config/BeanConfig.java
+++ src/main/java/bot/farm/blank/config/BeanConfig.java
@@ -1,6 +1,7 @@
 package bot.farm.blank.config;
 
 import bot.farm.blank.Bot;
+import bot.farm.blank.service.MessageService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,10 +10,11 @@ import org.springframework.context.annotation.Configuration;
 @RequiredArgsConstructor
 public class BeanConfig {
     private final BotConfig botConfig;
+    private final MessageService messageService;
 
     @Bean
     Bot bot() {
-        Bot bot = new Bot();
+        Bot bot = new Bot(messageService);
         bot.setBotUsername(botConfig.getBotName());
         bot.setBotToken(botConfig.getBotToken());
 
